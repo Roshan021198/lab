424826629e994b03cae33ea2013e31ceb46fa70d
unknown <tathagat.dalai@gmail.com>
2021-01-16 11:25:06 +0530

    updation of fornt page

---
+++ static/assets/js/news.js
@@ -0,0 +1,18 @@
+window.addEventListener('scroll',keveal);
+function keveal(){
+    var keveals = document.querySelectorAll('.keveal');
+    
+
+    for(var i=0; i< keveals.length; i++){
+        var windowheight=window.innerHeight;
+        var kevealtop =keveals[i].getBoundingClientRect().top;
+        var kevealpoint =150;
+
+        if(kevealtop < windowheight - kevealpoint){
+            keveals[i].classList.add('active')
+        }
+        else{
+            keveals[i].classList.remove('active')
+        }
+    }
+}
